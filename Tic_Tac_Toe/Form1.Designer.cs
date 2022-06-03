f2a73b27cf83b594c4490c52596adc007720bfb7
Joshua Sundiam <104367499+JoshuaSundiam@users.noreply.github.com>
2022-06-03 23:10:20 +0800

    buttons command

--- Tic_Tac_Toe/Form1.Designer.cs
+++ Tic_Tac_Toe/Form1.Designer.cs
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.b1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b1.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b1.Location = new System.Drawing.Point(91, 181);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(198, 139);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 58.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(26, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(561, 157);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tic Tac Toe";
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b2.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b2.Location = new System.Drawing.Point(295, 181);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(198, 139);
+            this.b2.TabIndex = 2;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b3.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b3.Location = new System.Drawing.Point(499, 181);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(198, 139);
+            this.b3.TabIndex = 3;
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b4.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b4.Location = new System.Drawing.Point(91, 326);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(198, 139);
+            this.b4.TabIndex = 4;
+            this.b4.UseVisualStyleBackColor = false;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b5.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b5.Location = new System.Drawing.Point(295, 326);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(198, 139);
+            this.b5.TabIndex = 5;
+            this.b5.UseVisualStyleBackColor = false;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b6.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b6.Location = new System.Drawing.Point(499, 326);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(198, 139);
+            this.b6.TabIndex = 6;
+            this.b6.UseVisualStyleBackColor = false;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b7
+            // 
+            this.b7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b7.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b7.Location = new System.Drawing.Point(91, 471);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(198, 139);
+            this.b7.TabIndex = 7;
+            this.b7.UseVisualStyleBackColor = false;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b8.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b8.Location = new System.Drawing.Point(295, 471);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(198, 139);
+            this.b8.TabIndex = 8;
+            this.b8.UseVisualStyleBackColor = false;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.b9.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b9.Location = new System.Drawing.Point(499, 471);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(198, 139);
+            this.b9.TabIndex = 9;
+            this.b9.UseVisualStyleBackColor = false;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.images;
+            this.ClientSize = new System.Drawing.Size(969, 732);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button b1;
+        private Label label1;
+        private Button b2;
+        private Button b3;
+        private Button b4;
+        private Button b5;
+        private Button b6;
+        private Button b7;
+        private Button b8;
+        private Button b9;
     }
 }
--- Tic_Tac_Toe/Form1.cs
+++ Tic_Tac_Toe/Form1.cs
@@ -2,9 +2,120 @@ namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
     {
+        bool turn = true;
+        int turn_count = 0;
         public Form1()
         {
+
             InitializeComponent();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            Button b1 = (Button)sender;
+            if (turn)
+                b1.Text = "X";
+            else
+                b1.Text = "O";
+            turn = !turn;
+            b1.Enabled = false;
+
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            Button b2 = (Button)sender;
+            if (turn)
+                b2.Text = "X";
+            else
+                b2.Text = "O";
+            turn = !turn;
+            b2.Enabled = false;
+
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            Button b3 = (Button)sender;
+            if (turn)
+                b3.Text = "X";
+            else
+                b3.Text = "O";
+            turn = !turn;
+            b3.Enabled = false;
+
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            Button b4 = (Button)sender;
+            if (turn)
+                b4.Text = "X";
+            else
+                b4.Text = "O";
+            turn = !turn;
+            b4.Enabled = false;
+
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            Button b5 = (Button)sender;
+            if (turn)
+                b5.Text = "X";
+            else
+                b5.Text = "O";
+            turn = !turn;
+            b5.Enabled = false;
+
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            Button b6 = (Button)sender;
+            if (turn)
+                b6.Text = "X";
+            else
+                b6.Text = "O";
+            turn = !turn;
+            b6.Enabled = false;
+
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            Button b7 = (Button)sender;
+            if (turn)
+                b7.Text = "X";
+            else
+                b7.Text = "O";
+            turn = !turn;
+            b7.Enabled = false;
+
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            Button b8 = (Button)sender;
+            if (turn)
+                b8.Text = "X";
+            else
+                b8.Text = "O";
+            turn = !turn;
+            b8.Enabled = false;
+
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            Button b9 = (Button)sender;
+            if (turn)
+                b9.Text = "X";
+            else
+                b9.Text = "O";
+            turn = !turn;
+            b9.Enabled = false;
+
         }
     }
 }
